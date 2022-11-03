8981a288c3c67b290b12c8738f47bbafb13c4bcb
sorozco <sorozco@tsomobile.com>
2022-11-03 14:22:39 -0500

    2nd commit

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -5,6 +5,18 @@ import { Avatar } from "./Avatar";
 export default {
   title: "Design System/Avatar",
   component: Avatar,
+  /*
+   * More on Storybook argTypes at:
+   * https://storybook.js.org/docs/react/api/argtypes
+   */
+  argTypes: {
+    size: {
+      control: {
+        type: "select",
+      },
+      options: ["tiny", "small", "medium", "large"],
+    },
+  },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
@@ -37,16 +49,16 @@ export const Initials = (args) => (
 );
 
 export const Loading = (args) => (
-    <div>
-      <Avatar {...args} size="large" />
-      <Avatar {...args} size="medium" />
-      <Avatar {...args} size="small" />
-      <Avatar {...args} size="tiny" />
-    </div>
-  );
-  Loading.args = {
-    loading: true,
-  };
+  <div>
+    <Avatar {...args} size="large" />
+    <Avatar {...args} size="medium" />
+    <Avatar {...args} size="small" />
+    <Avatar {...args} size="tiny" />
+  </div>
+);
+Loading.args = {
+  loading: true,
+};
 
 export const Large = (args) => (
   <div>
@@ -59,3 +71,17 @@ export const Large = (args) => (
     />
   </div>
 );
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+/*
+ * More on args at:
+ * https://storybook.js.org/docs/react/writing-stories/args
+ */
+Controls.args = {
+  loading: false,
+  size: "tiny",
+  username: "Dominic Nguyen",
+  src: "https://avatars2.githubusercontent.com/u/263385",
+};
